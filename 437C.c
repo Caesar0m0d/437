da6a8db667ccd6f771d3da251af1219bc2bfa663
Caesar0m0d <Caesar0m0d@gmail.com>
2017-04-14 00:39:54 +0900

    437C.c

--- 437C.c
+++ 437C.c
@@ -5,7 +5,7 @@ typedef struct Node{
 int n,vv;
 int nx[1003];
 }h;
-
+	int cal(int n,h ** li);
 	int cre(int u,int v,h ** li,int * vi);
 	int main(){
 int n,m,vi[1000],u,v,l;
@@ -20,7 +20,7 @@ scanf("%d ",&vi[l]);
 scanf("%d %d",&u,&v);
 cre(u,v,li,vi);
 				}
-
+cal(n,li);
 
 return 0;
 		  }
@@ -36,6 +36,30 @@ h * new2 = (h *)malloc(sizeof(h));new2->n=v;li[v-1]=new2;
 temp=li[u-1];temp2=li[v-1];temp->nx[v-1]=1;temp2->nx[u-1]=1;temp->vv=vi[u-1];temp2->vv=vi[v-1];
 return 0;
 	 	 }
+	 int cal(int n,h ** li){
+h * temp;
+int tp=100000,tp2=0,l,l2,turn,fin,n1;//tp -> past, tp2 -> present
+n1 = n;
+		while(n1!=1){
+		for(l=0;l<n;l++){
+if(li[l]==0)break;
+temp = li[l];
+		for(l2=0;l2<n;l2++){
+if((temp->nx[l2])==1)tp2+=(li[l2]->vv);
+				   }
+if(tp>tp2){tp=tp2;turn=l;}
+				}
+	//complete the minimum value 
+fin+=tp;temp=li[turn];
+		for(l2=0;l2<n;l2++){
+if((temp->nx[l2])==1){li[l2]->nx[turn]=0;temp->nx[l2]=0;}	
+				   }
+li[turn]=0;
+n1--;
+			     }
+printf("%d\n",fin);
+return 0;
+			 	}
 /*
 typedef struct Node{
 int n,vv;
